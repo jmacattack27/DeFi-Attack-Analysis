1b7317c4e0037a0390a8518308caec6c61f23507
Jack McKay <jack.mckay@utoronto.ca>
2022-04-25 22:22:49 -0400

    added conclusion section, final updates

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,14 +1,9 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the data downloaded from DEFIYIELD's REKT database
+# Author: Jack McKay
+# Data: April 25 2022
+# Contact: jack.mckay@utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
 
 
 #### Workspace setup ####
